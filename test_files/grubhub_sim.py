82cca37d6f5c8122552809377f6cdc9fe0c68b3d
amadoood <amadod@bu.edu>
2025-11-11 12:10:03 -0500

    Added new test files

---
+++ test_files/grubhub_sim.py
@@ -0,0 +1,56 @@
+from datetime import datetime, timedelta
+import time
+import requests
+
+WEBHOOK_URL = "http://localhost:3000/api/grubhub/webhook"
+TIME_IN_BETWEEN = 2
+
+MOCK_ORDERS = [
+    {"orderId": "1001", "merchantName": "Saxbys", "status": "IN_PROGRESS", "orderTime": "2025-10-30T00:00:05Z", "MugExchange": "No"},
+    {"orderId": "1002", "merchantName": "Pavement", "status": "IN_PROGRESS", "orderTime": "2025-10-30T00:00:10Z", "MugExchange": "Yes"},
+    {"orderId": "1003", "merchantName": "Starbucks", "status": "IN_PROGRESS", "orderTime": "2025-10-30T00:00:15Z", "MugExchange": "Yes"},
+    {"orderId": "1004", "merchantName": "Saxbys", "status": "IN_PROGRESS", "orderTime": "2025-10-30T00:00:20Z", "MugExchange": "Yes"},
+    {"orderId": "1005", "merchantName": "Starbucks", "status": "IN_PROGRESS", "orderTime": "2025-10-30T00:00:25Z", "MugExchange": "No"},
+    {"orderId": "1006", "merchantName": "Pavement", "status": "IN_PROGRESS", "orderTime": "2025-10-30T00:00:30Z", "MugExchange": "No"},
+    {"orderId": "1007", "merchantName": "Starbucks", "status": "IN_PROGRESS", "orderTime": "2025-10-30T00:00:35Z", "MugExchange": "Yes"},
+    {"orderId": "1008", "merchantName": "Saxbys", "status": "IN_PROGRESS", "orderTime": "2025-10-30T00:00:40Z", "MugExchange": "Yes"},
+    {"orderId": "1009", "merchantName": "Pavement", "status": "IN_PROGRESS", "orderTime": "2025-10-30T00:00:45Z", "MugExchange": "No"},
+    {"orderId": "1010", "merchantName": "Starbucks", "status": "IN_PROGRESS", "orderTime": "2025-10-30T00:00:50Z", "MugExchange": "Yes"},
+    {"orderId": "1011", "merchantName": "Pavement", "status": "IN_PROGRESS", "orderTime": "2025-10-30T00:00:30Z", "MugExchange": "Yes"},
+    {"orderId": "1012", "merchantName": "Starbucks", "status": "IN_PROGRESS", "orderTime": "2025-10-30T00:00:35Z", "MugExchange": "Yes"},
+    {"orderId": "1013", "merchantName": "Saxbys", "status": "IN_PROGRESS", "orderTime": "2025-10-30T00:00:40Z", "MugExchange": "Yes"},
+    {"orderId": "1014", "merchantName": "Pavement", "status": "IN_PROGRESS", "orderTime": "2025-10-30T00:00:45Z", "MugExchange": "No"},
+    {"orderId": "1015", "merchantName": "Starbucks", "status": "IN_PROGRESS", "orderTime": "2025-10-30T00:00:50Z", "MugExchange": "No"}
+]
+
+def generate_mock_orders(time_between_orders):
+    max_time, elapsed_time = 0, 0
+    for i in range(10):
+        time.sleep(time_between_orders)
+        start_time = time.time()
+        try:
+            response = requests.post(WEBHOOK_URL, json=MOCK_ORDERS[i])
+            print(f"Sent order {i} to node.js server with status {response.status_code}")
+        except Exception as e:
+            print(f"Failed sending order via webook: {e}")
+        end_time = time.time()
+
+        elapsed_time = end_time - start_time
+        if elapsed_time > max_time:
+            max_time = elapsed_time
+    return max_time
+
+
+
+if __name__ == "__main__":
+    max_latency = generate_mock_orders(TIME_IN_BETWEEN)
+    time.sleep(5)
+
+    stats = requests.get("http://localhost:3000/test/stats").json()
+
+    expected_mug_exchange = sum(1 for orders in MOCK_ORDERS if orders["MugExchange"] == "Yes")
+
+    assert stats["totalReceived"] == len(MOCK_ORDERS)
+    assert stats["totalMugExchange"] == expected_mug_exchange
+
+    print(f"🎉 {TIME_IN_BETWEEN} second test passed successfully with a maximum latency of {max_latency:.2f} seconds!")
---
+++ test_files/server_test.js
@@ -0,0 +1,63 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const PORT = 3000;
+
+let MOCK_ORDERS = [];
+
+app.use(express.json());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+    },
+});
+
+io.on('connection', (socket) => {
+    console.log("Frontend connected: ", socket.id);
+
+    socket.on('disconnect', () => {
+        console.log('Frontend disconnected: ', socket.id);
+    });
+});
+
+//Testing portion
+let stats = { totalReceived: 0, totalMugExchange: 0 };
+app.get('/test/stats', (req, res) => {
+    res.json(stats);
+})
+
+app.post('/api/grubhub/webhook', (req, res) => {
+    const order = req.body;
+
+    console.log("Webhook received", order);
+    stats["totalReceived"] += 1;
+
+    if(req.body.MugExchange == "Yes") {
+    MOCK_ORDERS.push(order);
+
+    const emittedAt = Date.now();
+
+    const payload = {
+        ...order,
+        emittedAt,
+    };
+
+    io.emit("orderUpdate", payload);
+    console.log("Sent payload to websocket");
+    stats["totalMugExchange"] += 1;
+    }
+
+    res.status(200).json({message: "Webhook received"})
+});
+
+app.get('/', (req, res) => {
+    res.send("Mug Exchange server!");
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
